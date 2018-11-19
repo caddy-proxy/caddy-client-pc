d182a817d491d36445313dc4e3f339382f846b4a
fake ccp <fakeccp123@gmail.com>
2018-11-19 13:39:28 -0800

    log to file

--- js/logger.js
+++ js/logger.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const messages = require('./messages.js');
 
-//const logTerminal = 'file';
-const logTerminal = 'console';
+const logTerminal = 'file';
+//const logTerminal = 'console';
 module.exports = {
     //log: log string
     log : function(log) {
